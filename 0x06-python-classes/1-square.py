d06139abcbfb6c7c64f2e622bc6a9c7c7f97820e
Akeem Mudashiru <akeemmudash@gmail.com>
2023-11-22 00:27:15 +0100

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defining a class `Square`."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Method to initialize a new Square class.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
